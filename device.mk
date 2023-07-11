1b0ebc01ddf5bfa7053914221504d28e84f2fec1
SIDDHANT <siddhantt605@gmail.com>
2023-07-11 23:20:36 +0000

    Revert "Revert "veux: switch to prebuilt libdisplayconfig.qti.so""
    This reverts commit 3e7c6ddc4ee53a766ccb8cddf9ba2eaf549605ba.

--- device.mk
+++ device.mk
@@ -172,7 +172,6 @@ PRODUCT_PACKAGES += \
     memtrack.default
 
 PRODUCT_PACKAGES += \
-    libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
